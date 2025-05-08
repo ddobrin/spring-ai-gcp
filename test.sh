36fc241f9ec685a343d4b7f508fdeec28c96590f
Josh Long <54473+joshlong@users.noreply.github.com>
2025-05-08 18:26:19 +0100

    test

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+http :8080/jlong/inquire question=="do you have any neurotic dogs?"
+http :8080/jlong/inquire question=="fantastic. when can i schedule an appointment to pickup Prancer from the Mountain View location?"
+

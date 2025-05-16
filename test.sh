b83be59e8e35d0e83b2389ac3eea7117ad76a086
ddobrin <ddobrin@google.com>
2025-05-16 13:00:53 -0400

    corrected user name

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-http :8080/jlong/inquire question=="do you have any silly dogs?"
-http :8080/jlong/inquire question=="fantastic. when can i schedule an appointment to pickup Prancer from the Mountain View location?"
+http :8080/lee/inquire question=="do you have any silly dogs?"
+http :8080/lee/inquire question=="fantastic. when can i schedule an appointment to pickup Prancer from the Mountain View location?"
 
